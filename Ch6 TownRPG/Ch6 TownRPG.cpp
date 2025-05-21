630ce0f5c9a164771ec62eb8ba544e2a2b2f5cf6
ParkSeyang <ghlwkd14@naver.com>
2025-05-21 17:49:47 +0900

    (2025.05.21) 노션을 활용하여 게임 프로젝트 관리하기 TownRPG

---
+++ Ch6 TownRPG/Ch6 TownRPG.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "GameModel.h"
+
+/*
+* GameModel 만든 기능을 테스트 합니다.
+* BattleManager - Player, Monster
+*/
+
+int main()
+{
+    Player player(100, 10);
+    Monster monster(80, 5);
+
+    // BattleManager battlemanager(player, monster); battlemanager.Battle()
+
+    // 체력이 0보다 크면 반복해라. -> while문 탈출 조건을 먼저 작성해라.
+    
+    while (player.getHealth() > 0 && monster.getHealth() > 0)
+    {
+        player.attack(monster);
+        if (monster.getHealth() > 0)
+        {
+            monster.attack(player);
+        }
+    }
+
+    std::cout << "전투가 종료 되었습니다." << std::endl;
+}
+
